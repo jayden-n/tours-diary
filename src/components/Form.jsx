f8522a12d57ed081d9f2fbeeb8615ac641bca727
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-08-23 14:47:41 -0400

    implemented: fetching city data into the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,16 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './Form.module.css';
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 import BackButton from './BackButton';
+import { useUrlPosition } from '../hooks/useUrlPosition';
+import Message from './Message';
+import Spinner from './Spinner';
 
+// Emoji helper function
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
@@ -15,14 +19,53 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+
 function Form() {
+  const [lat, lng] = useUrlPosition();
   const navigate = useNavigate();
 
   const [cityName, setCityName] = useState('');
   const [country, setCountry] = useState('');
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState('');
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
+  const [emoji, setEmoji] = useState('');
+  const [geocodingError, setGeocodingError] = useState('');
 
+  useEffect(() => {
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true);
+        setGeocodingError('');
+
+        const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+        const data = await res.json();
+
+        // Error handling when user click on the river
+        if (!data.countryCode) {
+          throw new Error('Sorry :( That does not seem like a city...');
+        }
+        setCityName(data.city || data.locality || '');
+        setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
+      } catch (err) {
+        setGeocodingError(err.message);
+      } finally {
+        setIsLoadingGeocoding(false);
+      }
+    }
+    fetchCityData();
+    // actively changing the country name when click
+  }, [lat, lng]);
+
+  if (isLoadingGeocoding) {
+    return <Spinner />;
+  }
+
+  if (geocodingError) {
+    return <Message message={geocodingError} />;
+  }
   return (
     <form className={styles.form}>
       <div className={styles.row}>
@@ -32,7 +75,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,23 +10,38 @@ import {
 } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../context/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 const Map = () => {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lng, geolocationPosition.lat]);
+  }, [geolocationPosition]);
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition ? (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'use your position'}
+        </Button>
+      ) : (
+        ''
+      )}
       <MapContainer
         center={mapPosition}
         // center={[mapLat, mapLng]}
